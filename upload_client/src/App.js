2ea7a8e3f060288562b2688aa64a6cbc8000f1c3
Josh Bryson <josh@bryson.me.uk>
2019-08-14 19:04:15 +0100

    File DropArea created

--- upload_client/src/App.js
+++ upload_client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
-          <Route exact path="/" />
+          <Route exact path="/" component={MainPage} />
         </Switch>
     </Router>
   );
---
+++ upload_client/src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div className="App">
+            <div className="DropArea">
+                 <div className="Status">Drop Here</div> 
+            </div> 
+        </div> 
+    );
+};
+export default MainPage;
